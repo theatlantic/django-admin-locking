1eeff9a020a51cb02830a165adcc33d227d34346
David Krauth <dkrauth@theatlantic.com>
2020-07-14 17:08:00 -1000

    Update the tox and travis testing matrix to include Django 2.2 (plus grappelli), and remove all other unsupported versions

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import find_packages, setup
 
 setup(
     name='django-admin-locking',
-    version='1.7.0',
+    version='1.8.0',
     url='https://github.com/theatlantic/django-admin-locking',
-    download_url='https://github.com/theatlantic/django-admin-locking/tarball/v1.6',
+    download_url='https://github.com/theatlantic/django-admin-locking/tarball/v1.8.0',
     license='BSD',
     description='Prevents users from overwriting each others changes in Django.',
     author='Josh West',
@@ -19,13 +19,10 @@ setup(
         'Framework :: Django',
         'Framework :: Django :: 1.11',
         'Framework :: Django :: 2.0',
-        'Framework :: Django :: 2.1',
+        'Framework :: Django :: 2.2',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ]
 )
